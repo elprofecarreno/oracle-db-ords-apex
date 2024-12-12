c33335f4b7be1a261748d4bc47d45387b128085b
Cristian Carreño <elprofecarreno@gmail.com>
2024-12-11 23:28:23 -0300

    add data project

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 echo "CONN_STRING=sys/Oracle123@oracle-db-container:1521/xepdb1" > ords/ords_secrets/conn_string.txt
 rm -rf ords/ords_config/*
-docker rm -f $(docker ps | grep oracle-db-ords-apex | awk '{print $1}')
+docker rm -f $(docker ps -a | grep oracle-db-ords-apex | awk '{print $1}')
 docker compose up
