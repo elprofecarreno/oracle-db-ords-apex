bfba145d1b11e5f3ca9a5326a4192ef63783f306
Cristian Carreño <elprofecarreno@servipag.cl>
2025-10-16 00:41:13 -0300

    update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
-@echo off
-REM Crear el archivo conn_string.txt con los datos de conexión
-echo CONN_STRING=sys/Oracle123@oracle-db-container:1521/xepdb1 > ords\ords_secrets\conn_string.txt
+ @echo off
+REM Crear el archivo conn_string.txt con los datos de conexión SIN SALTO DE LÍNEA
+<NUL (SET /P X="CONN_STRING=sys/Oracle123@oracle-db-container:1521/xepdb1") > "ords\ords_secrets\conn_string.txt"
 
 REM Eliminar el contenido del directorio ords\ords_config (excepto el directorio mismo)
 rmdir /s /q ords\ords_config
@@ -10,4 +10,4 @@ REM Eliminar el contenedor con nombre que contiene "oracle-db-ords-apex"
 FOR /F "tokens=*" %%i IN ('docker ps -a --filter "name=oracle-db-ords-apex" --format "{{.ID}}"') DO docker rm -f %%i
 
 REM Iniciar los contenedores con docker-compose
-docker-compose up
+docker compose up
